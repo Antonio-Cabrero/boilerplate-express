a0c88f502d943ab6cc7aa7eb660335ed65178efd
Antonio Cabrero <cabrerotono@gmail.com>
2023-08-03 08:57:09 +0200

    lesson 6

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 let express = require("express");
 let app = express();
 console.log("Hello World");
@@ -6,6 +7,9 @@ app.get("/", (req, res) => {
 });
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/json", (req, res) => {
+	if (process.env.MESSAGE_STYLE === "uppercase") {
+		return res.json({ message: "Hello json".toUpperCase() });
+	}
 	res.json({ message: "Hello json" });
 });
 module.exports = app;
